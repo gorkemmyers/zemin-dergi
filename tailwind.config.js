6ab2fb4eb8cd89d27a0690d611a649a68da61cae
gorkemmyers <gorkemugurguler@gmail.com>
2026-08-26 13:53:12 +0300

    Update color palette in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,21 +9,17 @@ module.exports = {
     extend: {
       colors: {
         zemin: {
-          bej: '#FBF7EF',        // Ana Zemin (daha açık)
-          kagit: '#F2ECDF',      // Kart Zemin (bg'den bir ton koyu, katman hissi verir)
-          bordo: '#4E141E',
-          bordokoyu: '#380B13',
-          yesil: '#2D4F38',
-          yesilacik: '#3E654B',
-          metin: '#1F1E1B',
-          cizgi: '#DDD7CA',
-          // Kategori aksan renkleri
-          turuncu: '#C9713E',     // Felsefe
-          turuncukoyu: '#8F4E29',
-          hardal: '#B98B2E',      // Sosyoloji
-          hardalkoyu: '#805F1E',
-          mavi: '#3E5C74',        // Psikoloji
-          mavikoyu: '#2A3F50',
+          kagit: '#F7F2E7',
+          kagitkoyu: '#EFE7D6',
+          murekkep: '#211F1B',
+          murekkepacik: '#514C43',
+          kil: '#C1502E',
+          kilkoyu: '#8F3A20',
+          hardal: '#C99A2E',
+          hardalkoyu: '#93701D',
+          gece: '#33505A',
+          gecekoyu: '#22383F',
+          cizgi: '#E1D8C4',
         },
       },
       fontFamily: {
